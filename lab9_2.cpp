04c0738c825a81d9ff4486be3f1f9fab8fbf6462
naruemonlarpuangkom <narumon.02545@gmail.com>
2021-12-07 17:27:36 +0700

    lab9_2finished

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,28 +2,28 @@
 #include<iomanip>
 #include<string>
 using namespace std;
-
 //[Missing Code 1] Write definition of the function findGrade() here.
-char findGrade (float x){
-char g;
- if (x > 90.0)
- g = 'A';
-
- else if (x > 75.0 && x <=90.0)
- g = 'B';
-
- else if (x > 60.0 && x <= 75.0)
- g = 'C';
-
- else if (x > 45.0 && x <= 60.0)
- g = 'D';
-
- else g = 'F';
-
-return g;
- }
-
-
+char findGrade(double grade) {
+	if (grade > 90)
+	{
+		return 'A';
+	}
+	else if (grade > 75)
+	{
+		return 'B';
+	}
+	else if (grade > 60) 
+	{
+		return 'C';
+	}
+	else if ( grade > 45)
+	{
+		return 'D';
+	}
+	else{
+		return 'F';
+	}
+}
 int main(){
 	//Input the number of students
 	int N,i = 0;
@@ -36,6 +36,10 @@ int main(){
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
+		getline(cin,name[i]);
+		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		cout << "Score of student " << i+1 << ": ";
+		cin >> score[i];
 		//[Missing Code 3] Get score of the i-th students.
 		i++;
 	}
@@ -50,36 +54,5 @@ int main(){
 		i++;
 	} 
 	cout << "---------------------------------------------\n";
-
 	return 0;
 }
-
-
-    int N,i = 0;
-    cout << "Enter the number of students: ";
-    cin >> N;
-    string name[N];
-    float score[N];
-
-
-    while(i < N){
-        cout << "Name of student " << i+1 << ": ";
-        cin.ignore();
-        getline(cin,name[i]);
-        cout << "Score of student " << i+1 << ": ";
-        cin >> score[i];
-        i++;
-    }
-
-    i = 0;
-    cout << "---------------------------------------------\n";
-    cout << setw(25) << "Name" << setw(8) << "Score" << setw(8) << "Grade" << "\n";
-    cout << "---------------------------------------------\n";
-    while(i < N){
-        cout << setw(25) << name[i] << setw(8) << score[i] << setw(8) << findGrade(score[i]) << "\n";
-        i++;
-    } 
-    cout << "---------------------------------------------\n";
-
-    return 0;
-    }
