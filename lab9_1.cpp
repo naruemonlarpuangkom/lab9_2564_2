18df2b5dcea9b2928a00521a0f3d5c23adf9d697
naruemonlarpuangkom <narumon.02545@gmail.com>
2021-12-07 16:21:21 +0700

    lap9_1

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
   char rank;
@@ -13,5 +12,25 @@ int main()
     case 'C': cout << "You have received 2000 coins.\n";
     case 'D': cout << "You have received very KAK items.\n";
   }
+  //switch(rank){
+    //case 'S': cout << "You have received Super Ultra Rare Unit!!!\n";
+    //case 'A': cout << "You have received 5 gems.\n";
+    //case 'B': cout << "You have received 1 gems.\n";
+   // case 'C': cout << "You have received 2000 coins.\n";
+    //case 'D': cout << "You have received very KAK items.\n";
+  //}
+
+  if (rank == 'S'){
+    cout << "You have received Super Ultra Rare Unit!!!\n" << "You have received 5 gems.\n" << "You have received 1 gems.\n" << "You have received 2000 coins.\n" << "You have received very KAK items.\n";
+    }else if (rank == 'A'){
+      cout << "You have received 5 gems.\n" << "You have received 1 gems.\n" << "You have received 2000 coins.\n" << "You have received very KAK items."  ;
+      }else if (rank == 'B'){
+        cout << "You have received 1 gems.\n" << "You have received 2000 coins.\n" << "You have received very KAK items.\n" ;
+        }else if (rank == 'C'){
+          cout << "You have received 2000 coins.\n" << "You have received very KAK items.\n" ;
+            }else if (rank == 'D'){
+              cout << "You have received very KAK items.\n";
+            }
+
   return 0;
 }
